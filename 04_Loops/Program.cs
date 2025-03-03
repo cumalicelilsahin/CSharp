274dbd89494870dd9e2e99e342ac608b3f08bd8e
EXCALIBUR <EXCALIBUR@DESKTOP-L2DCMKI>
2025-03-03 15:07:48 +0300

    4

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region forDöngüsü
+            //int i;
+            //int j = 1;
+            //int k = 1;
+            //for (i = 0; i <= 10; i++)
+            //{
+            //    Console.Write(j + " ");
+            //    j += k;
+            //    k =j-k;
+            //}
+            #endregion
+
+            #region WhileDöngüsü
+
+            //int sayac = 0;
+            //int i = 1;
+            //int j = 1;
+            //while (i<=100)
+            //{
+            //    while (j<i)
+            //    {
+            //        if(i%j==0)
+            //        {
+            //            sayac++;
+            //        }
+            //        j++;
+            //    }
+            //    if (sayac==1)
+            //    {
+            //        Console.WriteLine(i + " ");
+            //    }
+            //    sayac = 0;
+            //    i++;
+            //    j = 1;
+            //}
+
+            #endregion
+
+
+        }
+    }
+}
