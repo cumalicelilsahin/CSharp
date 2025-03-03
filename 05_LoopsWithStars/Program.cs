a619384182f363ceeb5641bfe9a5e9641ced2f5c
EXCALIBUR <EXCALIBUR@DESKTOP-L2DCMKI>
2025-03-03 15:44:36 +0300

    5

---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region EşkenarÜçgen
+
+            //for(int i=0; i<4; i++)
+            //{
+            //    for (int k=3; k>=i; k--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int j = 0; j <= i; j++)
+            //    {
+            //        Console.Write(" *");
+            //    }
+            //    Console.WriteLine("");
+            //}
+
+            #endregion
+
+            #region DikÜçgen
+            
+            //for(int i = 0; i <= 4; i++)
+            //{
+            //    for(int j=0; j< i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            
+            #endregion
+
+
+            Console.Read();
+        }
+    }
+}
