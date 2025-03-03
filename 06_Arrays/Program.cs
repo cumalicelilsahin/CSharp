8c849a68e3ea96dc1de3bdcbbb71bab7f6a89852
EXCALIBUR <EXCALIBUR@DESKTOP-L2DCMKI>
2025-03-03 16:56:33 +0300

    Added array creation and sorting functionality.

---
+++ 06_Arrays/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region DiziOluşturma
+            //string[] takimlar = new string[5];
+            //takimlar[0] = "Fenerbahçe";
+            //takimlar[1] = "Malatyaspor";
+            //takimlar[3] = "Samsunspor";
+            //takimlar[4] = "Konyaspor";
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(takimlar[i]+" "+i);
+            //}
+
+            #endregion
+
+            #region DiziSiralama
+
+            //int hafiza = 0;
+            //int i;
+            //int[] dizi = { 1, 11, 51, 15, 26, 72, 68, 98, 45, 44, 10, 42 };
+            //for (i = 0; i < 12; i++)
+            //{
+            //    if (dizi[i] > hafiza)
+            //    {
+            //        hafiza = dizi[i];
+            //    }
+            //}
+            //Console.WriteLine(hafiza);
+            //i = 0;
+            //Array.Sort(dizi);
+            //while (i < dizi.Length)
+            //{
+            //    Console.WriteLine(dizi[i]);
+            //    i++;
+            //}
+            #endregion
+
+
+
+
+        }
+    }
+}
