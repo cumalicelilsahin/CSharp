7effd82901e5f5dbbd76fd035d1927f4508aba4d
EXCALIBUR <EXCALIBUR@DESKTOP-L2DCMKI>
2025-03-03 14:16:40 +0300

    3

---
+++ 03_MakingDecision/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_MakingDecision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region if
+
+            //Console.Write("Lütfen şifreyi giriiniz: ");
+            //string password = Console.ReadLine();
+            //if (password == "asd123")
+            //{
+            //    Console.WriteLine("Doğru, giriş yapılıyor.");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Şifre yanlış !");
+            //}
+            #endregion
+
+            #region Modİşlemleri
+
+            //Console.WriteLine("Sayı Giriniz: ");
+            //int sayi1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("2.Sayı Giriniz: ");
+            //int sayi2 = int.Parse(Console.ReadLine());
+
+            //int sonuc = sayi1 % sayi2;
+            //Console.WriteLine("Sonuç: " + sonuc);
+
+
+
+
+            #endregion
+
+            #region TavukDönerciMenüsü
+
+            //Console.WriteLine("-Menü-");
+            //Console.WriteLine("1: Yemek");
+            //Console.WriteLine("2: İçecek");
+            //Console.WriteLine("Değer giriniz: ");
+            //int deger = int.Parse(Console.ReadLine());
+            //if(deger==1 | deger == 2)
+            //{
+            //    if(deger==1)
+            //    {
+            //        Console.WriteLine("Tavuk Dürüm");
+            //    }
+            //    if (deger == 2)
+            //    {
+            //        Console.WriteLine("Ayran");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Yanlış Giriş !");
+            //}
+
+            #endregion
+
+            #region DönerciMenüsüAmaCaseİle
+            //Console.WriteLine("-MENÜ-");
+            //Console.WriteLine("1: Döner");
+            //Console.WriteLine("2: İçecek");
+
+            //int secim= int.Parse(Console.ReadLine());
+            //switch (secim)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Et");
+            //        Console.WriteLine("Tavuk");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Ayran");
+            //        Console.WriteLine("Şalgam");
+            //        break;
+            //}
+
+            #endregion
+
+
+
+            Console.Read();
+        }
+    }
+}
