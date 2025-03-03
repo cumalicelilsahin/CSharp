ffd5b682e3b92a6f2d3877b15f7fa674a8413816
EXCALIBUR <EXCALIBUR@DESKTOP-L2DCMKI>
2025-03-03 11:41:06 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _01_MainSubjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region YazdırmaKomutları
+            //Console.WriteLine("Hello World");
+            //Console.Write("Selamr");
+
+            //Console.WriteLine("*****MENU*****");
+            //Console.WriteLine("1.Çorba");
+            //Console.WriteLine("2.Yemek");
+            //Console.WriteLine("3.İçecek");
+            #endregion
+
+            #region StringDeğişkenler
+
+            //string
+            //Değişken_türü değişken_adi;
+
+            //string name;
+            //name = "CumaliCelil";
+            //Console.WriteLine(name);
+
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone, customerEmail;
+            //customerName = "Ömer";
+            //customerSurname = "Akcan";
+            //customerPhone = "+90 555 255 55 55";
+            //customerEmail = "asd@asd.com";
+            //Console.WriteLine(customerName + " " + customerSurname + " " + customerPhone + " " + customerEmail);
+
+            //customerName = "Nurkan";
+            //customerSurname = "Akcn";
+            //customerPhone = "+90 550 255 55 55";
+            //customerEmail = "wasd@asd.com";
+            //Console.WriteLine(customerName + " " + customerSurname + " " + customerPhone + " " + customerEmail);
+
+
+            #endregion
+
+            #region IntDeğişkenler
+
+            //int
+            int number = 24;
+            Console.WriteLine(number);
+
+            
+
+            #endregion
+
+
+            Console.Read();
+        }
+    }
+}
