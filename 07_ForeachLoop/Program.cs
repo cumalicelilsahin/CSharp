dc4aebb5932d6ed562451d599759920db6db4dbc
EXCALIBUR <EXCALIBUR@DESKTOP-L2DCMKI>
2025-03-04 10:46:05 +0300

    Implement foreach loop for iterating through lists and arrays

---
+++ 07_ForeachLoop/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ForeachKullanımı
+
+            //foreach (var item in collection)
+            //{
+            //    //1:DeğişkenTürü
+            //    //2:DeğişkenAdı
+            //    //3:In
+            //    //4:Liste, Koleksiyon, Dizi
+
+            //}
+
+            #endregion
+
+            #region DizininSayilariToplami
+            //int[] dizi = { 4, 55, 552, 5, 28281, 215, 125, 1235, 1285, 2155 };
+            //int sonuc = 0;
+            //foreach (int gez in dizi)
+            //{
+            //    sonuc +=gez;
+            //    Console.WriteLine(gez);
+
+            //}
+            //Console.WriteLine(sonuc);
+
+            #endregion
+
+            #region ListeOluşturma
+
+            //List<int> sayilar = new List<int>()
+            //{
+            //    1,2,3,3,4,7
+            //};
+
+            //foreach(int gez in sayilar)
+            //{
+            //    Console.WriteLine(gez);
+            //}
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
