b818f3da907919edce126b476d14bb9f6e3d9a30
EXCALIBUR <EXCALIBUR@DESKTOP-L2DCMKI>
2025-03-04 12:09:32 +0300

    Define methods and parameters in C#

---
+++ 08_Methods/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region MetotOluşturma
+
+            ////╔╦ Değer Döndürmeyen Void Method ╔╦
+            //void EkranCiktisi()
+            //{
+            //    Console.WriteLine("Bu Bir Mesajmı ?");
+            //}
+
+            ////╔╦ Değer Döndürmeyen String Parametreli Method ╔╦
+            //void EkranCiktisi(string isim)
+            //{
+            //    Console.WriteLine($"İ  İsmi: {isim}");
+
+            //}
+            //Console.WriteLine("İsmi ne olsun? ");
+            //string ism = Console.ReadLine();
+            //EkranCiktisi(ism);
+
+            ////╔╦ Değer Döndürmeyen İnt Parametreli Method ╔╦
+            //void Metot(int a, int b, int c)
+            //{
+            //    int avg = (a + b + c) / 3;
+            //    Console.WriteLine($"Ortalaması {avg}'dir");
+            //}
+
+            //Console.WriteLine("1. sayıyı girin: ");
+            //int x = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("2. sayıyı girin: ");
+            //int y = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("3. sayıyı girin: ");
+            //int z = int.Parse(Console.ReadLine());
+
+            //Metot(x, y,z);
+
+
+            ////╔╦ Değer Döndüren Method ╔╦
+            //string IsimSoyisim()
+            //{
+            //    return "Marko Arnautovic";
+            //}
+
+            //IsimSoyisim();
+
+            ////╔╦ Değer Döndüren Method ╔╦
+            //string IsimSoyisim(string isim, string soyisim)
+            //{
+            //    string Info = "İsim: " + isim + " - Soyisim: " + soyisim;
+            //    return Info;
+            //}
+            //string x, y;
+            //Console.Write("İsim: ");
+            //x = Console.ReadLine();  
+            //Console.Write("Soyisim: ");
+            //y = Console.ReadLine();
+
+            //IsimSoyisim(x, y);
+
+            #endregion
+
+            Console.Read();
+
+        }
+    }
+}
