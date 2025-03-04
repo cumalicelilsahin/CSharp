e306c6637bce258193a32eb351a94ca2f117ed6d
EXCALIBUR <EXCALIBUR@DESKTOP-L2DCMKI>
2025-03-04 14:54:17 +0300

    Implement connection to SQL database.

---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+            SqlConnection connection = new SqlConnection("Server=DESKTOP-L2DCMKI;Database=CSharpDB;Trusted_Connection=True;Integrated Security=true;TrustServerCertificate=true");
+            connection.Open();
+
+            SqlCommand sqlCommand = new SqlCommand("select * from FirstTable", connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            connection.Close();
+
+            foreach(DataRow row in dataTable.Rows)
+            {
+                foreach(var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
+
+            //---
+            Console.Read();
+        }
+    }
+}
